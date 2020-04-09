d33bef02eb9b3ac0d9bb02d4354c8ab9d4626f7a
David Tong <ytong82@users.noreply.github.com>
2020-04-09 05:46:28 -0500

    Update values.go

--- values.go
+++ values.go
@@ -9,6 +9,6 @@ func main() {
 	fmt.Println("7.0 / 3.0 = ", 7.0/3.0)
 
 	fmt.Println(true && false)
-    fmt.Println(true || false)
-    fmt.Println(!true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 }
