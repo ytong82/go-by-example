30573d631b14d60cfbfd80296fcfca9a1600e4f1
David Tong <ytong82@users.noreply.github.com>
2020-04-09 05:50:11 -0500

    Update arrays.go

--- arrays.go
+++ arrays.go
@@ -5,8 +5,9 @@ import "fmt"
 func main() {
 	var a [5]int
 	fmt.Println("emp:", a)
+	
 	a[4] = 100
-    fmt.Println("set:", a)
+	fmt.Println("set:", a)
 	fmt.Println("get:", a[4])
 	fmt.Println("len:", len(a))
 
